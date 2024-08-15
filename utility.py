dc2ccfb64739507476b00a432510897a32b1708a
Brian-Hansen-Turton <britine@me.com>
2024-08-15 23:07:11 +0100

    comment

--- utility.py
+++ utility.py
@@ -18,6 +18,7 @@ def add_or_update_item(list, name, price, quanity):
     new_item = {order_item_name_key: name,
                 order_item_price_key: price,
                 order_item_quanity_key: quanity}
+    """This helper function will add a new order, or update the quanity if already ordered. """
     
     for item in list:
         if item[order_item_name_key] == new_item[order_item_name_key]:
